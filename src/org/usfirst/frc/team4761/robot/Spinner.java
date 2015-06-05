d8228181e595ab7c54f400d6bc0f65589c672819
Simon Andrews <simon@simonandrews.org>
2015-06-04 20:22:59 -0400

    Update spinner now that I have a better idea of how it will work

--- src/org/usfirst/frc/team4761/robot/Spinner.java
+++ src/org/usfirst/frc/team4761/robot/Spinner.java
@@ -1,16 +1,23 @@
 package org.usfirst.frc.team4761.robot;
 
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Victor;
 
 public class Spinner {
-	private Talon spinner = new Talon(4); //TODO: Change this if needed when the real robot is ready
+	private Victor spinner1 = new Victor(4); //TODO: Change this if needed when the real robot is ready
+	private Victor spinner2 = new Victor(5);
 	public void spin(double speed) {
 		if(speed < 0 || speed > 1) {
 			throw new IllegalArgumentException("Invalid speed for spinner (must be 0..1)");
 		}
-		spinner.set(speed);
+		spinner1.set(speed);
+		spinner2.set(speed);
 	}
 	public void stop() {
-		spinner.stopMotor();
+		spinner1.set(0);
+		spinner2.set(0);
+	}
+	public void emergencyStop() {
+		spinner1.stopMotor();
+		spinner2.stopMotor();
 	}
 }
