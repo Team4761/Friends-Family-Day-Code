4843bf1d55bce0ac64f6bd98ea7fc5bc697cfe7a
Simon Andrews <simon@simonandrews.org>
2015-06-04 20:40:27 -0400

    Add constructors to spinner

--- src/org/usfirst/frc/team4761/robot/Spinner.java
+++ src/org/usfirst/frc/team4761/robot/Spinner.java
@@ -3,8 +3,19 @@ package org.usfirst.frc.team4761.robot;
 import edu.wpi.first.wpilibj.Victor;
 
 public class Spinner {
-	private Victor spinner1 = new Victor(4); //TODO: Change this if needed when the real robot is ready
-	private Victor spinner2 = new Victor(5);
+	private Victor spinner1;
+	private Victor spinner2;
+	
+	public Spinner(int port1, int port2) {
+		spinner1 = new Victor(port1);
+		spinner2 = new Victor(port2);
+	}
+	
+	public Spinner(Victor victor1, Victor victor2) {
+		spinner1 = victor1;
+		spinner2 = victor2;
+	}
+	
 	public void spin(double speed) {
 		if(speed < 0 || speed > 1) {
 			throw new IllegalArgumentException("Invalid speed for spinner (must be 0..1)");
@@ -12,10 +23,12 @@ public class Spinner {
 		spinner1.set(speed);
 		spinner2.set(speed);
 	}
+	
 	public void stop() {
 		spinner1.set(0);
 		spinner2.set(0);
 	}
+	
 	public void emergencyStop() {
 		spinner1.stopMotor();
 		spinner2.stopMotor();
